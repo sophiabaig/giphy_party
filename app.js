da59f8b363b75994eabf97a3d010f06bbf875ab7
Sophia Baig <sophiabaig@fb.com>
2022-06-27 16:41:21 -0700

    fixed

--- app.js
+++ app.js
@@ -3,27 +3,24 @@ const api_key = "3NdWmu4yjtFddAHoAfUWUKeWwGoc9jWG"
 const limit = 9
 const rating = "g"
 const lang = "en"
-const pages = 0
-const offset = 0;
-var value = ""
+var pages = 0
+var offset = 0;
+var val = ""
 
 // QUERY SELECTORS
 const form = document.querySelector("#form")
-const searchBar = document.querySelector("#search-bar")
 const userInput = document.querySelector("#user-input")
-const submit = document.querySelector("#submit")
 const gifResults = document.querySelector("#gif-results")
 const loadMore = document.querySelector("#load-more")
 
 async function getResults(val) {
-    const offset = pages*limit
-    const response = await fetch(`http://api.giphy.com/v1/gifs/search?api_key=${MY_API_KEY}&q=${val}$&limit=${offset}`)
+    offset = pages*limit
+    const response = await fetch(`http://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${val}$&limit=${offset}`)
     const jsonResponse = await response.json()
     const data = jsonResponse.data
     displayResults(data)
     if (pages == 1) {
-        // loadMore.classList.remove("hidden")
-        loadMore.classList.toggle("hidden", false)
+        loadMore.classList.remove("hidden")
     }
 }
 function displayResults(data) {
@@ -41,15 +38,14 @@ function displayResults(data) {
 form.addEventListener('submit', (event) => {
     pages = 1;
     event.preventDefault();
-    value = userInput.value 
-    console.log(val)
-    getResults(value)
+    val = userInput.value 
+    getResults(val)
 })
 
 // show more
 loadMore.addEventListener('click', (event) => {
     pages++
-    getResults(value)
+    getResults(val)
 })
 
 
